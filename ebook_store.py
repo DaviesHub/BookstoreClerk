8ac02a73e654decb948397233e6eb0eba1d2005d
DaviesHub <davieschuks.a@gmail.com>
2022-11-15 20:15:21 +0200

    Create book instances

--- ebook_store.py
+++ ebook_store.py
@@ -33,7 +33,15 @@ db_cursor.execute('''
 ''')
 conn.commit()
 
-# Initialize records
+# Records are initialized by creating multiple instances of the book class
+book_1 = Book(3001, "A Tale of Two Cities", "Charles Dickens", 30)
+book_2 = Book(3002, "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 40)
+book_3 = Book(3003, "The Lion, the Witch and the Wardrobe", "C. S. Lewis", 25)
+book_4 = Book(3004, "The Lord of the Rings", "J. R. R. Tolkien", 37)
+book_5 = Book(3005, "Alice in Wonderland", "Lewis Caroll", 12)
+book_6 = Book(3006, "Animal Farm", "George Orwell", 32)
+book_7 = Book(3007, "The Stand", "Stephen King", 22)
+
 
 
 
